3d89a99c930dfe31b113d0fd9dad070ee92140ef
navneet2508 <navneety250801@gmail.com>
2022-11-15 04:25:23 +0530

    Added Thunderclient collections to manage requests

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
+// Create a user :POST "/api/auth/createuser". No login required
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
@@ -12,24 +13,41 @@ router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // If there are errors,return bad requests and errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: "Please enter a unique value of email",
-          message: err.message,
-        });
+
+    // Chck whether the user with this email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      console.log(user);
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
       });
+
+      // .then((user) => res.json(user))
+      // .catch((err) => {
+      //   console.log(err);
+      //   res.json({
+      //     error: "Please enter a unique value of email",
+      //     message: err.message,
+      //   });
+      // });
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occured");
+    }
   }
 );
 
