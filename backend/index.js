06b91b0e289911cc9b35454416ccf58528d4ae7b
navneet2508 <navneety250801@gmail.com>
2022-11-14 18:10:20 +0530

    Stored data into database

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,13 @@ const express = require("express");
 
 connectToMongo();
 const app = express();
-const port = 8080;
+const port = 3000;
+
+app.use(express.json());
+
+// Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.get("/", (req, res) => {
   res.send("Hello Navneet!");
