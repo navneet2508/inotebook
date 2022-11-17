619ea2f45cc79cfa47b8c70230a94fe200611aba
navneet2508 <navneety250801@gmail.com>
2022-11-18 02:45:43 +0530

    UseContextHook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Home, About, Navbar } from "./components";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </>
+    <div>
+      <NoteState>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </NoteState>
+    </div>
   );
 }
 
