c81fe02bda41144f92aa690bf6eeefebff08b175
navneet2508 <navneety250801@gmail.com>
2022-11-21 03:32:50 +0530

    Fetching Notes from Context

--- backend/index.js
+++ backend/index.js
@@ -17,5 +17,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`iNotebook backend listening on port http://localhost:${port}`);
 });
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,8 +2,58 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
+  const notesInitial = [
+    {
+      _id: "63740204cd0b1719e3e41bca",
+      user: "63738c94be229705f3c59d73",
+      title: "My title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2022-11-15T21:17:56.094Z",
+      __v: 0,
+    },
+    {
+      _id: "63740204cd0b1719e3e41bcc",
+      user: "63738c94be229705f3c59d73",
+      title: "My title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2022-11-15T21:17:56.325Z",
+      __v: 0,
+    },
+    {
+      _id: "63740204cd0b1719e3e41bce",
+      user: "63738c94be229705f3c59d73",
+      title: "My title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2022-11-15T21:17:56.524Z",
+      __v: 0,
+    },
+    {
+      _id: "63740205cd0b1719e3e41bd0",
+      user: "63738c94be229705f3c59d73",
+      title: "My title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2022-11-15T21:17:57.325Z",
+      __v: 0,
+    },
+    {
+      _id: "63740205cd0b1719e3e41bd2",
+      user: "63738c94be229705f3c59d73",
+      title: "My title",
+      description: "Please wake up early",
+      tag: "personal",
+      date: "2022-11-15T21:17:57.488Z",
+      __v: 0,
+    },
+  ];
+  const [notes, setNotes] = useState(notesInitial);
   return (
-    <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>
+    <NoteContext.Provider value={{ notes, setNotes }}>
+      {props.children}
+    </NoteContext.Provider>
   );
 };
 
