e4cb05d2814708fb41cfd8da227eeca9a6880c27
navneet2508 <navneety250801@gmail.com>
2023-08-06 19:10:40 +0530

    mongodb configured

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectToMongo = () => {
-  mongoose.connect("mongodb://localhost:27017", () => {
-    console.log("Mongo connected successfully");
-  });
+const connectToMongo = async () => {
+  await mongoose.connect(
+    "mongodb+srv://navneetrao:rckVp4p5yZbvQxbd@personal.vgiveoi.mongodb.net/?retryWrites=true&w=majority",
+    () => {
+      console.log("Mongo connected successfully");
+    }
+  );
 };
 
 module.exports = connectToMongo;
